4bb9a1bafcd2d7be3735d8b894c8b090a8a27cb5
Angelie <ramda2@farmingdale.edu>
2018-05-14 06:30:42 -0400

    Update
    Fixed all issues except ContactView TextView being hidden by keyboard

--- SeniorProject/TODO.swift
+++ SeniorProject/TODO.swift
@@ -8,30 +8,5 @@
 
 /*
  URGENT TODO:
-> Show Password Feature Adds space making login incorrect
-> activiy indicators
- ==================================
-> Design themes
-> Optional - Slide across login & registration
-
- URGENT:
- PAYPAL CREDENTIALS:
- TEST USER
-angelieramdial@gmail.com
- testuser123
-
- > Finish Settings Page
- >shawn added validation on email when registering
- >Settings tab -> About US, Contact US
-
- QUESTIONS:
- ACTIVATE_ACCOUNT.PHP ?
- 
- BUGS
- - DECREASE PRODUCT NO WORKING
- -  STARTS AT 1 INSTEAD OF CURRENT NUMBER IN CART
-    -Number changed at update should update quantity of product
- -trello
- -test corrections
 
  */
